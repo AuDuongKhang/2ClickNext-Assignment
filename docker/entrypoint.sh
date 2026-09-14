ff3a217efb6d51ed9f93e1fb5558e7473a61527f
Au Duong Khang <duongkhang2100@gmail.com>
2026-09-15 00:27:45 +0700

    fix: forward Docker entrypoint arguments

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,4 +3,9 @@ set -eu
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+
+if [ "$#" -gt 0 ]; then
+    exec "$@"
+fi
+
 exec gunicorn config.wsgi:application --bind 0.0.0.0:3000
